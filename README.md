9db4f4648a94f6cf83e0769745b5c0386779361d
Mo Aziz <mohammed4685@outlook.com>
2024-07-27 15:46:50 +0100

    First commit - added main.py + basic README

---
+++ README.md
@@ -0,0 +1,7 @@
+# ForenImage
+Tool for Master's Dissertation 2024
+## Requirements
+To be added
+
+## Documentation
+To be added
---
+++ main.py
@@ -0,0 +1,8 @@
+#Masters Disserattion Project - u5580118
+#Effectiveness of Picture Forensic Tools
+#The following is a tool that will provide the following features:
+
+#MODULES
+
+def main():
+    print("Hello world")
